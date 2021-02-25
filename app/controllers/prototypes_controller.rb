9ae716595c7dd2191a7b7b96b86c0b22143de77d
sk918 <skmtmt96918@gmail.com>
2021-02-26 04:25:32 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,65 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :edit, :destroy]
+  before_action :set_prototype, only: [:show, :edit]
+  before_action :move_to_index, except: [:index, :show]
+
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+  end
+
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Prototype.find(params[:id]).destroy
+    redirect_to prototypes_path
+  end
+
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+end
